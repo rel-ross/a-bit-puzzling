04460d6bdd6858aeed54926a13766b912c4de399
bwesterg <benton.westergaard@gmail.com>
2021-01-06 08:56:18 -0700

    work on welcome page and selection menu

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,25 +1,33 @@
 class CLI   
-    def clear 
+    def clear
         system("clear")
     end
-    
     def start_puzzle_app
         splash_page()
     end
-
     def splash_page
         puts "Everyday I'm puzzlin'"
         gets
         welcome_user()
     end
-
     def welcome_user
         clear()
         puts "Hello, puzzle friend! What may I call you?"
         @name = gets.chomp
+        initial_menu_selection()
     end
-
+    def prompt
+        prompt = TTY::Prompt.new()
+    end
+    def initial_menu_selection
+        prompt()
+        menu_items = ["See a list of available puzzles", "See puzzles in your posession"]
+        prompt.select("What would you like to do?", menu_items)
+    end
+    
     def available_puzzles
         puts "The following puzzles are available:"
+        prompt()
+        prompt.select("Would you like to select any of the puzzles below?", ["test1", "test2"])
     end
 end
